fbabc400cef588defb2b91bef24f3e975abdca48
Andrew Bullock <trullock@gmail.com>
2022-07-19 13:25:47 +0100

    Added AMT22 demo

--- AMT22/AMT22.ino
+++ AMT22/AMT22.ino
@@ -52,8 +52,7 @@
 #define RES14           14
 
 /* SPI pins */
-#define ENC_0            2
-#define ENC_1            3
+#define ENC_0            10
 #define SPI_MOSI        11
 #define SPI_MISO        12
 #define SPI_SCLK        13
@@ -65,14 +64,12 @@ void setup()
 	pinMode(SPI_MOSI, OUTPUT);
 	pinMode(SPI_MISO, INPUT);
 	pinMode(ENC_0, OUTPUT);
-	pinMode(ENC_1, OUTPUT);
 	
 	//Initialize the UART serial connection for debugging
 	Serial.begin(BAUDRATE);
 
 	//Get the CS line high which is the default inactive state
 	digitalWrite(ENC_0, HIGH);
-	digitalWrite(ENC_1, HIGH);
 
 	//set the clockrate. Uno clock rate is 16Mhz, divider of 32 gives 500 kHz.
 	//500 kHz is a good speed for our test environment
@@ -98,7 +95,6 @@ void loop()
 
 	//if you want to set the zero position before beggining uncomment the following function call
 	//setZeroSPI(ENC_0);
-	//setZeroSPI(ENC_1);
 
 	//once we enter this loop we will run forever
 	while(1)
@@ -131,39 +127,9 @@ void loop()
 	    Serial.write(NEWLINE);
 	  }
 
-	  //////////again for second encoder//////////////////////////////
-	  
-	  //set attemps counter at 0 so we can try again if we get bad position    
-	  attempts = 0;
-
-	  //this function gets the encoder position and returns it as a uint16_t
-	  //send the function either res12 or res14 for your encoders resolution
-	  encoderPosition = getPositionSPI(ENC_1, RES14); 
-
-	  //if the position returned was 0xFFFF we know that there was an error calculating the checksum
-	  //make 3 attempts for position. we will pre-increment attempts because we'll use the number later and want an accurate count
-	  while (encoderPosition == 0xFFFF && ++attempts < 3)
-	  {
-	    encoderPosition = getPositionSPI(ENC_1, RES14); //try again
-	  }
-
-	  if (encoderPosition == 0xFFFF) //position is bad, let the user know how many times we tried
-	  {
-	    Serial.print("Encoder 1 error. Attempts: ");
-	    Serial.print(attempts, DEC); //print out the number in decimal format. attempts - 1 is used since we post incremented the loop
-	    Serial.write(NEWLINE);
-	  }
-	  else //position was good, print to serial stream
-	  {
-	    
-	    Serial.print("Encoder 1: ");
-	    Serial.print(encoderPosition, DEC); //print the position in decimal format
-	    Serial.write(NEWLINE);
-	  }
-
 	  //For the purpose of this demo we don't need the position returned that quickly so let's wait a half second between reads
 	  //delay() is in milliseconds
-	  delay(500);
+	  //delay(500);
 	}
 }
 
